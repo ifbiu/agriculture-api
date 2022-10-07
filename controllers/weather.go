8dab7f1d63723eee7d73794304712baebcc4e210
Candide <biuvip@163.com>
2022-10-08 07:37:03 +0800

    weather add city

--- controllers/weather.go
+++ controllers/weather.go
@@ -16,24 +16,41 @@ type WeatherController struct {
 }
 
 type Daily struct {
-	TempMax string `json:"tempMax"`
-	TempMin string `json:"tempMin"`
-	IconDay string `json:"iconDay"`
-	TextDay string `json:"textDay"`
-	FxDate string `json:"fxDate"`
-	Weekday int `json:"weekday"`
+	TempMax       string `json:"tempMax"`
+	TempMin       string `json:"tempMin"`
+	IconDay       string `json:"iconDay"`
+	TextDay       string `json:"textDay"`
+	FxDate        string `json:"fxDate"`
+	Weekday       int    `json:"weekday"`
 	WeekdayFormat string `json:"weekdayFormat"`
 }
 
 type weatherResultStruct struct {
-	Code string `json:"code"`
+	Code  string  `json:"code"`
 	Daily []Daily `json:"daily"`
 }
 
 func (this *WeatherController) Get() {
-	var location = "101080601"
 	var key = "20b4d42efc414cd79fd73b7840604190"
-	response, err := http.Get("https://devapi.qweather.com/v7/weather/7d?location="+location+"&key="+key)
+	var city = this.GetString("city")
+	if city == "" {
+		this.Data["json"] = map[string]string{
+			"code":  "500",
+			"error": "missing parameter city",
+		}
+		this.ServeJSON()
+		return
+	}
+	location := switchCityToLocation(city)
+	if city == "nofound" {
+		this.Data["json"] = map[string]string{
+			"code":  "500",
+			"error": "city is no found",
+		}
+		this.ServeJSON()
+		return
+	}
+	response, err := http.Get("https://devapi.qweather.com/v7/weather/7d?location=" + location + "&key=" + key)
 	if err != nil {
 		log.Println("http get error")
 		return
@@ -56,10 +73,10 @@ func (this *WeatherController) Get() {
 		log.Println("json transformation error")
 		return
 	}
-	if weatherData.Code!="200" {
+	if weatherData.Code != "200" {
 		this.Data["json"] = map[string]string{
-			"code":weatherData.Code,
-			"err":"服务异常！",
+			"code": weatherData.Code,
+			"err":  "服务异常！",
 		}
 		this.ServeJSON()
 		return
@@ -67,15 +84,15 @@ func (this *WeatherController) Get() {
 	t := time.Now()
 	weekday := int(t.Weekday())
 	for i, daily := range weatherData.Daily {
-		fxDateAll := strings.Split(daily.FxDate,"-")
-		if len(fxDateAll)!=3 {
+		fxDateAll := strings.Split(daily.FxDate, "-")
+		if len(fxDateAll) != 3 {
 			log.Println("fxDate split error")
 			return
 		}
 		if fxDateAll[2][0] == '0' {
 			fxDateAll[2] = fxDateAll[2][1:]
 		}
-		weatherData.Daily[i].FxDate = fxDateAll[2]+"日"
+		weatherData.Daily[i].FxDate = fxDateAll[2] + "日"
 		// 获取周几
 		weatherData.Daily[i].Weekday = weekday
 		switch weekday {
@@ -96,12 +113,43 @@ func (this *WeatherController) Get() {
 
 		}
 		// 星期加加
-		if weekday+1>6 {
-			weekday=0
-		}else{
-			weekday = weekday+1
+		if weekday+1 > 6 {
+			weekday = 0
+		} else {
+			weekday = weekday + 1
 		}
 	}
 	this.Data["json"] = weatherData
 	this.ServeJSON()
+}
+
+func switchCityToLocation(city string) string {
+	var res = "nofound"
+	switch city {
+	case "huhehaote":
+		res = "101080101"
+	case "baotou":
+		res = "101080201"
+	case "wulanchabu":
+		res = "101080405"
+	case "tongliao":
+		res = "101080501"
+	case "chifeng":
+		res = "101080601"
+	case "eerduosi":
+		res = "101080701"
+	case "bayannaoer":
+		res = "101080811"
+	case "xilinguole":
+		res = "101080902"
+	case "hulunberer":
+		res = "101081013"
+	case "xinganmeng":
+		res = "101081108"
+	case "alashan":
+		res = "101081213"
+	case "wuhai":
+		res = "101080301"
+	}
+	return res
 }
